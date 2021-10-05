f7cf2b687ecc7874a8cae081b91bede4417eba04
Rafael Gouveia <rafael.gouveia@impar.com.br>
2021-10-05 10:53:50 -0300

    CSS and JS challenge

---
+++ src/webparts/helloWorld/components/Mockup.ts
@@ -0,0 +1,27 @@
+interface ListData {
+    Title: string;
+    Done: boolean;
+}
+
+export default [
+    {
+        Title: "Lavar a louça",
+        Done: false
+    },
+    {
+        Title: "Varrer a cozinha",
+        Done: false
+    },
+    {
+        Title: "Limpar o banheiro",
+        Done: false
+    },
+    {
+        Title: "Levar água pra galera",
+        Done: false
+    },
+    {
+        Title: "Descascar o aipim",
+        Done: false
+    }
+] as ListData[];
